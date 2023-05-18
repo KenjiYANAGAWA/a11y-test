95093e3d3d2accf8d772289cacd6c0c1cbf4af5b
Celso Yamashita <ctyamashita@hotmail.com>
2023-05-18 21:09:25 +0900

    2A issue - wip

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -11,12 +11,38 @@ const breakIcon = (selector) => {
     })
 }
 
+const removeElements = (selector) => {
+  const els = document.querySelectorAll(selector);
+  els.forEach(el => el.remove());
+}
+
+const modifyAriaLabel = (selector, newContent = '') => {
+  const els = document.querySelectorAll(selector);
+  els.forEach(el => el.setAttribute('aria-label', newContent));
+}
+
 window.onload = () => {
   console.log('loaded');
 
   if (location.pathname == '/products/tanida-gaming-dekstop') {
-    const iconSelector = ['.icon-picto-box', '.icon-picto-customer-support', '.icon-picto-coupon', '.icon-picto-lock']
+    const iconSelector = [
+      '.icon-picto-box',
+      '.icon-picto-customer-support',
+      '.icon-picto-coupon',
+      '.icon-picto-lock'
+    ]
     iconSelector.forEach(selector => breakIcon(selector));
+  } else if (location.pathname == '/') {
+    // removing titles from payment methods list on footer
+    const elementsToRemove = [
+      '.footer__payment-icons svg title'
+    ]
+    elementsToRemove.forEach((selector) => {
+      removeElements(selector);
+      // removing aria-labelledby
+      document.querySelectorAll(selector).forEach(el => el.parentElement.removeAttribute('aria-labelledby'));
+      modifyAriaLabel(selector, 'card')
+    });
   }
 
 }
