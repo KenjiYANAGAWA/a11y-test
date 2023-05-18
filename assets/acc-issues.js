50575a656a985dde94bca4400dc528a4df7eb5e9
Celso Yamashita <ctyamashita@hotmail.com>
2023-05-18 20:05:50 +0900

    4A issue - icon

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -1,1 +1,22 @@
 console.log('connected');
+
+const breakIcon = (selector) => {
+  const icons = document.querySelectorAll('');
+    icons.forEach((icon) => {
+      icon.removeAttribute('focusable');
+      icon.setAttribute('tabindex', 0);
+      const label = icon.parentElement.nextElementSibling.firstElementChild.firstElementChild.innerText;
+      icon.setAttribute('aria-label', label);
+    })
+}
+
+window.onload = () => {
+  console.log('loaded');
+
+  if (location.pathname == '/products/tanida-gaming-dekstop') {
+    const iconSelector = ['.icon-picto-box', '.icon-picto-customer-support', '.icon-picto-coupon', 'icon-picto-lock']
+    breakIcons();
+
+  }
+
+}
