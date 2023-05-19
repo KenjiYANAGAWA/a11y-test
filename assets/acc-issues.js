70c4df2f2a4dcadd90d8a3c49cdd3b6249693ab4
Celso Yamashita <ctyamashita@hotmail.com>
2023-05-20 00:14:37 +0900

    30A issue - revisit

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -31,7 +31,7 @@ window.onload = () => {
     const buyBtn = document.querySelector('.buy-buttons button');
     buyBtn.addEventListener('click', () => {
       console.log('click');
-      document.body.focus();
+      document.querySelector('a').focus();
       console.log(window.activeElement);
     });
 
