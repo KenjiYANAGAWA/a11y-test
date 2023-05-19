0f6ffdccf1ca2e056260efea50a0d4d795335660
Celso Yamashita <ctyamashita@hotmail.com>
2023-05-19 19:45:58 +0900

    30A issue - revisit

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -28,8 +28,8 @@ window.onload = () => {
   if (location.pathname == '/products/tanida-gaming-dekstop') {
     // messing focus from pop up when add to cart;
     const buyBtn = document.querySelector('.buy-buttons .button');
-    buyBtn.addEventListener('click', (e) => {
-      document.focus()
+    buyBtn.addEventListener('click', () => {
+      document.body.focus();
     });
 
     // Changing average rating display
