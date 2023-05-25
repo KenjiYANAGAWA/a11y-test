afad06c37d47d113a8d2b97bd98ebf7f45e7bcff
Celso Yamashita <ctyamashita@hotmail.com>
2023-05-25 18:14:52 +0900

    issue 16A

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -123,5 +123,16 @@ window.onload = () => {
     setTimeout(() => {
       iconSelector.forEach(selector => breakIcon(selector));
     }, 1000);
+
+    const contactForm = ocument.querySelector('#contact-form form');
+    contactForm.lastElementChild.classList = '';
+    contactForm.lastElementChild.innerHTML =
+      `<div style="display:flex; gap: 8px"><div class="checkbox-container"><input id="checkbox-template--18980282171676__contact---contactbody-" class="checkbox" type="checkbox" name="contact[body]" value="1" required=""></div><span>Do you agree to privacy policy?</span></div>
+      <div class="justify-self-start">
+        <button type="submit" class="button button--xl" is="custom-button"><div>Send message</div><span class="button__loader">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span></button></div>`;
   }
 }
