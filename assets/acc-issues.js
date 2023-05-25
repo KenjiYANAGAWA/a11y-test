54b40c658fbadac28cf8feaec5e03b158f014c4d
Celso Yamashita <ctyamashita@hotmail.com>
2023-05-25 17:08:53 +0900

    removing custom cursors

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -23,7 +23,7 @@ const convertNumberToStars = (rating) => {
 
 window.onload = () => {
   // removing custom cursor from cart
-  document.querySelector('show-close-cursor').classList.remove('show-close-cursor');
+  document.querySelector('.show-close-cursor').classList.remove('show-close-cursor');
 
   // removing custom cursor from carousels
   const customCursors = document.querySelectorAll('.slideshow__cursor');
