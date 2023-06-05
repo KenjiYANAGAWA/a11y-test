2002eb4a616e8d99354e31d5eab208fe6419eab3
Celso Yamashita <ctyamashita@hotmail.com>
2023-06-05 17:43:33 +0900

    adding issues to cart page

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -191,8 +191,8 @@ window.onload = () => {
         expandBtn.click();
       }
       // after adding zipcode move to estimate button automatically
-      if (document.activeElement == zipCodeInput && zipCodeInput.value.length > 5) {
-        zipCodeInput.value = zipCodeInput.value.slice(0,4);
+      if (document.activeElement == zipCodeInput && zipCodeInput.value.length >= 5) {
+        zipCodeInput.value = zipCodeInput.value.slice(0,5);
         estimateBtn.focus();
       }
     })
