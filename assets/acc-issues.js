0963737f5a99601dccd4cdd1c42f5230d63106da
Celso Yamashita <ctyamashita@hotmail.com>
2023-06-05 20:17:34 +0900

    adding issues to product pages

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -202,10 +202,8 @@ window.onload = () => {
     const infoSeparatorEl = document.querySelector('.product-info__separator');
     infoSeparatorEl.insertAdjacentHTML('afterend', '<div style="height: 60px; display: flex;"><a href="/pages/contact" class="spr-button spr-button-primary button button-primary btn btn-primary button--primary button--xl">Get help</a></div>');
     document.querySelector('video-media').play();
-    document.querySelector('video-media').style.overflow = 'hidden'
     setTimeout(() => {
       document.querySelector('.video-play-button').click();
-      document.querySelector('iframe').style.scale = 1.1;
     }, 3000);
     setTimeout(() => {
       document.querySelector('.video-play-button').click();
