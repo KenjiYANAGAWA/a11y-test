8188ce1b7a0e4eb050cd348f1146636edd3debca
Celso Yamashita <ctyamashita@hotmail.com>
2023-06-05 17:44:48 +0900

    adding issues to cart page

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -142,18 +142,18 @@ window.onload = () => {
 
   } else if (location.pathname == '/cart') {
     //trap focus on checkout button
-    // const checkoutBtn = document.querySelector('.cart-form button[type=submit]');
-    // checkoutBtn.onfocus = () => {
-    //   document.addEventListener('keydown', (e) => {
-    //     checkoutBtn.focus();
-    //     e.preventDefault();
-    //   })
-    // }
+    const checkoutBtn = document.querySelector('.cart-form button[type=submit]');
+    checkoutBtn.onfocus = () => {
+      document.addEventListener('keydown', (e) => {
+        checkoutBtn.focus();
+        e.preventDefault();
+      })
+    }
 
     // moving to checkout in 30s
-    // setTimeout(() => {
-    //   checkoutBtn.click();
-    // }, 30000);
+    setTimeout(() => {
+      checkoutBtn.click();
+    }, 30000);
 
     // doesn't announce collapsable estimate shipping
     const estimateShippingEl = document.querySelector('.cart-order__summary details');
