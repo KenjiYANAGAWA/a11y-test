b4d08931028cf1ab08e81222e9250f93fd1e1d1d
Celso Yamashita <ctyamashita@hotmail.com>
2023-06-06 17:12:10 +0900

    adding issue video second attempt

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -237,8 +237,8 @@ window.onload = () => {
     var player;
       function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
-          height: '100%',
-          width: '100%',
+          height: '390',
+          width: '640',
           videoId: 'uo_8BX-iihE',
           events: {
             'onReady': onPlayerReady
