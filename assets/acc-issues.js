af7072340c2a3f757b4e05a6beb019fa717c0eeb
Celso Yamashita <ctyamashita@hotmail.com>
2023-06-13 19:38:00 +0900

    popup list issues initial push

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -593,7 +593,7 @@ listIssue.style.boxShadow = '0 0 20px 20px rgba(0,0,0,.3)';
 listIssue.style.zIndex  = 1;
 
 // adding popup issue list
-document.addEventListener('keyup', (e) => {
+document.addEventListener('keydown', (e) => {
 
   if (comboKey.includes(e.key)) return;
 
