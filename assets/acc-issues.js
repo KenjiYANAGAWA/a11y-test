9aa4cbfe0d901b6829043092322a94fe2d842559
Celso Yamashita <ctyamashita@hotmail.com>
2023-06-13 20:38:39 +0900

    popup list issues initial push

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -64,6 +64,32 @@ const issueListArray = () => {
   }
 }
 
+function trapFocus(element) {
+  const focusableEls = element.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
+  const firstFocusableEl = focusableEls[0];
+  const lastFocusableEl = focusableEls[focusableEls.length - 1];
+  const KEYCODE_TAB = 9;
+
+  element.addEventListener('keydown', function(e) {
+    var isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+
+    if (!isTabPressed) {
+      return;
+    }
+
+    if ( e.shiftKey ) /* shift + tab */ {
+      if (document.activeElement === firstFocusableEl) {
+        lastFocusableEl.focus();
+          e.preventDefault();
+        }
+      } else /* tab */ {
+      if (document.activeElement === lastFocusableEl) {
+        firstFocusableEl.focus();
+          e.preventDefault();
+        }
+      }
+  });
+}
 
 // low color contrast cart and social media links
 const lowContrastColor = "rgb(var(--text-color) / .4)"
