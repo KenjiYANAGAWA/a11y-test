a71b6b7e58f5d39a80d4fae3bdcb481772864b75
Celso Yamashita <ctyamashita@hotmail.com>
2023-06-13 17:03:34 +0900

    adding payment page

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -32,7 +32,7 @@ window.onload = () => {
     if (btns.length > 0) {
       btns.forEach((btn) => {
         btn.insertAdjacentHTML('afterend', `<a class="button" href='/pages/checkout'>Checkout</a>`);
-        btn.style.display = 'none';
+        // btn.style.display = 'none';
       })
     }
   })
