af4126d95526af9e2b494eaaafbe77b0afdfb893
Celso Yamashita <ctyamashita@hotmail.com>
2023-06-14 14:17:32 +0900

    adding warning to advertisement page

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -51,22 +51,24 @@ window.onload = () => {
   swapDiv(headerLogo);
 
   // disabling buy it now btn
-  const buyNowBtn = document.querySelector('.shopify-payment-button__button');
-  if (buyNowBtn) {
-    const newBuyNowBtn = document.createElement('a');
-    newBuyNowBtn.href = '#'
-    newBuyNowBtn.style.display = 'inline-block';
-    newBuyNowBtn.style.textAlign = 'center';
-    newBuyNowBtn.style.fontWeight = 600;
-    newBuyNowBtn.style.borderRadius = '200px'
-    newBuyNowBtn.style.padding = '17.2px 40px'
-    newBuyNowBtn.style.color = 'white';
-    newBuyNowBtn.style.background = 'black';
-    newBuyNowBtn.style.width = '100%';
-    newBuyNowBtn.style.height = '100%';
-    newBuyNowBtn.innerText = 'Buy it now';
-    buyNowBtn.parentElement.replaceChild(newBuyNowBtn, buyNowBtn);
-  }
+  setTimeout(() => {
+    const buyNowBtn = document.querySelector('.shopify-payment-button__button');
+    if (buyNowBtn) {
+      const newBuyNowBtn = document.createElement('a');
+      newBuyNowBtn.href = '#'
+      newBuyNowBtn.style.display = 'inline-block';
+      newBuyNowBtn.style.textAlign = 'center';
+      newBuyNowBtn.style.fontWeight = 600;
+      newBuyNowBtn.style.borderRadius = '200px'
+      newBuyNowBtn.style.padding = '17.2px 40px'
+      newBuyNowBtn.style.color = 'white';
+      newBuyNowBtn.style.background = 'black';
+      newBuyNowBtn.style.width = '100%';
+      newBuyNowBtn.style.height = '100%';
+      newBuyNowBtn.innerText = 'Buy it now';
+      buyNowBtn.parentElement.replaceChild(newBuyNowBtn, buyNowBtn);
+    }
+  }, 700);
 
   // removed custom cursor from cart, search and product page
   // changed theme.css -> .popover::part(overlay) and .drawer.show-close-cursor::part(overlay) selectors
