2bfec1373a660d86e34c1319e43f5e1d37c1e195
Celso Yamashita <ctyamashita@hotmail.com>
2023-06-14 10:46:31 +0900

    organizing code for better issue list

--- assets/acc-issues-helpers.js
+++ assets/acc-issues-helpers.js
@@ -1,4 +1,4 @@
-export function trapFocus(element) {
+function trapFocus(element) {
   const focusableEls = element.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled]), [tabindex]:not([tabindex="-1"])');
   const firstFocusableEl = focusableEls[0];
   const lastFocusableEl = focusableEls[focusableEls.length - 1];
@@ -25,6 +25,6 @@ export function trapFocus(element) {
   });
 }
 
-export function swapDiv(elem) {
+function swapDiv(elem) {
   elem.parentNode.insertBefore(elem, elem.parentNode.firstChild);
 }
