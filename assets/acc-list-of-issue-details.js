80f8efa82b7372f627dbb24c993338b9aa319e91
Kenji Yanagawa <kenchibi@gmail.com>
2023-06-16 18:02:30 +0900

    35mm

--- assets/acc-list-of-issue-details.js
+++ assets/acc-list-of-issue-details.js
@@ -46,7 +46,8 @@ const issueListObj = {
   '/products/mc100-wireless-charge-pad-gunmetal-aluminum-black-coated-canvas': [],
   '/products/3-5mm-to-3-5mm-audio-cable-black': [
     ['4.1.2 Name, Role, Value', 'ARIA5: Using WAI-ARIA state and property attributes to expose the state of a user interface component','Using aria-hidden="true" on a focusable image **This works very well with our gaming headphones.** inside a product description.'],
-    ['4.1.1 Parsing', 'ARIA10: Using aria-labelledby to provide a text alternative for non-text content', '**mg20-gaming-galactic-white** image has an aria-labelledby attribute references an ID that doesn\'t exist.']
+    ['4.1.1 Parsing', 'ARIA10: Using aria-labelledby to provide a text alternative for non-text content', '**mg20-gaming-galactic-white** image has an aria-labelledby attribute references an ID that doesn\'t exist.'],
+    ['3.2.4 Consistent Identification', 'F31: Failure of Success Criterion 3.2.4 due to using two different labels for the same function on different Web pages within a set of Web pages', '**MW50+** has **Get help** button that goes to **Contact**. Even though header and footer buttons with same destination say **Contact**']
   ],
   '/products/usb-c-to-3-5mm-audio-cable-black': [
     ['2.4.2 Page Titled','G127: Identifying a Web page\'s relationship to a larger collection of Web pages','product **USB-C To 3.5mm Audio Cable (Black)** and **Headphone Stand (Black)** pages have the title "product"']
