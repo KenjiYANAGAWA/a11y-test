9b09160fcc0bda92e8817388bd2609df3690baf1
Celso Yamashita <ctyamashita@hotmail.com>
2023-06-22 14:17:03 +0900

    adding issues to credit card payment

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -424,7 +424,7 @@ window.onload = () => {
       if (validCardNumber && validCardName && validCardCode) {
         e.target.submit();
       } else {
-        document.querySelector('.credit-card-info').insertAdjacentHTML('beforebegin', '<p>Credit card validation failed. Please try again.</p>')
+        document.querySelector('label[for="credit_card_payment"] .error-msg').innerHTML = 'Credit card validation failed. Please try again.'
       }
     }
 
--- assets/acc-set-up.js
+++ assets/acc-set-up.js
@@ -263,7 +263,8 @@ const accSetUp = () => {
       method.addEventListener('change', (e) => {
         if (e.target.checked && e.target.id == 'credit_card_payment') {
           e.target.parentElement.parentElement.insertAdjacentHTML('afterend',
-            `<div class="credit-card-info">
+            `<div class="error-msg"></div>
+            <div class="credit-card-info">
               <div class="checkout-input-container">
                 <label for="card-number" class="checkout-input-label">
                   <span>Card number</span>
