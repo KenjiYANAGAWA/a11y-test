8251f5f1764d7317d587e88ce2f19eeec129a9bc
Celso Yamashita <ctyamashita@hotmail.com>
2023-06-29 12:26:05 +0900

    hotspot issue

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -111,7 +111,7 @@ window.onload = () => {
     })
 
     document.addEventListener('keyup', (e) => {
-      if (previousFocusEl.classList.contains('hot-spot__dot') && e.key != 'Tab') {
+      if (previousFocusEl.classList.contains('hot-spot__dot') && ['Escape', ' ', 'Enter'].includes(e.key)) {
         previousFocusEl.focus();
       }
     })
