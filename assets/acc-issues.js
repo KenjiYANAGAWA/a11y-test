61af6614d734f93db6c3870a002c778d4317459f
Celso Yamashita <ctyamashita@hotmail.com>
2023-06-29 16:22:47 +0900

    breaking focustrap

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -148,7 +148,7 @@ window.onload = () => {
           const newCartDrawer = cartDrawer.cloneNode(true);
 
           cartDrawer.parentElement.replaceChild(newCartDrawer, cartDrawer)
-        }, 1000);
+        }, 1200);
 
         btn.focus();
       })
