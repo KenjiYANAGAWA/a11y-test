2177c4e749ae032af7a5df38708bfbf218414c3f
Celso Yamashita <ctyamashita@hotmail.com>
2023-06-29 12:32:24 +0900

    hotspot issue

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -111,7 +111,7 @@ window.onload = () => {
     })
 
     document.addEventListener('keyup', (e) => {
-      console.log(hotspots, previousFocusEl);
+      console.log(Array(hotspots), previousFocusEl);
       console.log(Array(hotspots).includes(previousFocusEl));
       if (Array(hotspots).includes(previousFocusEl) && ['Escape', ' ', 'Enter'].includes(e.key)) {
         previousFocusEl.focus();
