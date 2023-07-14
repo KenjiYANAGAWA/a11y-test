e2537ab5cdc0366503d78379b99b5db52684f2e1
Celso Yamashita <ctyamashita@hotmail.com>
2023-07-14 14:44:20 +0900

    filter issue

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -598,8 +598,9 @@ window.onload = () => {
           var maxPrice = Number(priceRangeInputs[1].value)
           var productCards = document.querySelectorAll('.product-card');
           productCards.forEach((product)=>{
-            var productPrice = Number(product.querySelector('.text-subdued').innerText.split('\n$')[1].replaceAll(',', ''))
-            if (productPrice > maxPrice || productPrice < minPrice) {
+            var productPrice = product.querySelector('.text-subdued').innerText.split('\n$')[1]
+            var price = Number(productPrice.replaceAll(',', ''))
+            if (price > maxPrice || price < minPrice) {
               product.style.display = 'none'
             } else {
               product.style.display = 'content'
