3970fa0eb42f5467330389426fa164d63013e32b
Celso Yamashita <ctyamashita@hotmail.com>
2023-09-05 15:55:53 +0900

    fixing small issues

--- assets/acc-issues-functions.js
+++ assets/acc-issues-functions.js
@@ -1,26 +1,26 @@
 var removeFooterLink = (href) => {
-  document.querySelector(`.footer__wrapper li:has(a[href="${href}"])`).remove();
+    document.querySelector(`.footer__wrapper .footer__block--menu li> a[href="${href}"]`).remove();
 }
 
 // function to generate link to WCAG techniques based on the name in item[1]
 function generateTechniqueLink(item) {
-  var input = item[1];
-  var code = input.substring(0, input.indexOf(':'));
-  var key = code.charAt(0);
+    var input = item[1];
+    var code = input.substring(0, input.indexOf(':'));
+    var key = code.charAt(0);
 
-  var linkMap = {
-    A: 'https://www.w3.org/WAI/WCAG21/Techniques/aria/',
-    F: 'https://www.w3.org/WAI/WCAG21/Techniques/failures/',
-    G: 'https://www.w3.org/WAI/WCAG21/Techniques/general/',
-    h: 'https://www.w3.org/WAI/WCAG21/Techniques/general/',
-    H: 'https://www.w3.org/WAI/WCAG21/Techniques/html/',
-    S: 'https://www.w3.org/WAI/WCAG21/Techniques/client-side-script/',
-    C: 'https://www.w3.org/WAI/WCAG21/Techniques/css/'
-  };
+    var linkMap = {
+        A: 'https://www.w3.org/WAI/WCAG21/Techniques/aria/',
+        F: 'https://www.w3.org/WAI/WCAG21/Techniques/failures/',
+        G: 'https://www.w3.org/WAI/WCAG21/Techniques/general/',
+        h: 'https://www.w3.org/WAI/WCAG21/Techniques/general/',
+        H: 'https://www.w3.org/WAI/WCAG21/Techniques/html/',
+        S: 'https://www.w3.org/WAI/WCAG21/Techniques/client-side-script/',
+        C: 'https://www.w3.org/WAI/WCAG21/Techniques/css/'
+    };
 
-  if (linkMap.hasOwnProperty(key)) {
-    return linkMap[key] + code;
-  } else {
-    return '';
-  }
+    if (linkMap.hasOwnProperty(key)) {
+        return linkMap[key] + code;
+    } else {
+        return '';
+    }
 }
