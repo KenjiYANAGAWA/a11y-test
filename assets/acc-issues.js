a3bc9bd2c20f69a812cb6c946b6284658a37e6c2
Celso Yamashita <ctyamashita@hotmail.com>
2023-09-22 16:19:31 +0900

    fixed small bugs

--- assets/acc-issues.js
+++ assets/acc-issues.js
@@ -58,6 +58,15 @@ window.onload = () => {
         document.title = 'product'
     }
 
+    if (currentLocation.includes('/search?')) {
+      try {
+        document.querySelector('form.main-search-form input[type="search"]').removeAttribute('aria-label');
+      } catch (error) {
+        console.log('icon search issue');
+        console.log(error)
+      }
+    }
+
     if (['/collections/headphones', '/collections/earphones', '/collections/speakers', '/collections/accessories'].includes(currentLocation)) {
         document.querySelector('.collection__top-bar').remove();
         document.querySelector('.collection__facets').remove();
